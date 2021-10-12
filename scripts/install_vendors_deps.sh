e9538f66f1c2816af293e85d6c0da6f20430036b
Ulrik Strid <ulrik.strid@outlook.com>
2021-10-12 15:27:46 +0200

    submodules should "just work"

--- scripts/install_vendors_deps.sh
+++ scripts/install_vendors_deps.sh
@@ -2,18 +2,6 @@
 set -e
 set -x
 
-# NEW-PROTOCOL-TEMPORARY
-cd ./vendors
-if [ ! -d "./tezos" ]
-then git clone git@gitlab.com:tezos/tezos.git ./tezos;
-fi
-cd ./tezos
-git fetch
-git checkout v11.0-rc1
-git submodule init
-cd ../..
-# NEW-PROTOCOL-TEMPORARY
-
 # Install local dependencies
 # opam install -y --deps-only --with-test --locked ./ligo.opam
 
