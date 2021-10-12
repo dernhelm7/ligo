be2db4fe948747af80b9f8d443110c41d9d2f002
Melwyn Saldanha <melwyn95@gmail.com>
2021-10-12 15:04:29 +0530

    wip: handle case-case(return-return)

--- src/test/integration_tests.ml
+++ src/test/integration_tests.ml
@@ -3423,6 +3423,10 @@ let switch_cases_jsligo ~raise ~add_warning () : unit =
   let _ = expect_eq ~raise program "case_case_break_return" (e_int 1) (e_string "Hello World ???") in
   let _ = expect_eq ~raise program "case_case_break_return" (e_int 2) (e_string "Hello !!!") in
   let _ = expect_eq ~raise program "case_case_break_return" (e_int 3) (e_string "Hello  ???") in
+
+  let _ = expect_eq ~raise program "case_case_return_return" (e_int 1) (e_string "Hello World") in
+  let _ = expect_eq ~raise program "case_case_return_return" (e_int 2) (e_string "Hello !!!") in
+  let _ = expect_eq ~raise program "case_case_return_return" (e_int 3) (e_string "Hello  ???") in
   ()
 
 let tuple_fun_religo ~raise ~add_warning () : unit =
